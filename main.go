7c63bd46fd03465dc59e240ebaa5370df45ac341
Nishant Giri <nishantgiri19722000@gmail.com>
2023-12-14 21:39:07 +0530

    main.go file modified : typo correction

--- main.go
+++ main.go
@@ -6,5 +6,5 @@ import "fmt"						/* import is used to include code from other packages.
 									Here fmt stands for formating for input and output. */
 
 func main() {						// using "main" is to declare the code lines are executable.
-	fmt.Println("Hello World!")		//Hence there will be no need to call function explicitly for each line of exection.
-}
+	fmt.Println("Hello World!")		//Hence there will be no need to call function explicitly for each line of execution.
+}̥
