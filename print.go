a25df59ccf697a318ae3458158c85e63241756b7
Nishant Giri <nishantgiri19722000@gmail.com>
2023-12-14 22:23:18 +0530

    print.go : file-add

---
+++ print.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main(){
+	
+	var a int = 1
+	var b int = 2
+	var c int = 3
+	var d int = 4
+
+	fmt.Println(" a :",a, "\n b :",b, "\n c :",c, "\n d :",d)
+
+	fmt.Println()
+
+	fmt.Printf(" a : %d\n b : %d\n c : %d\n d : %d\n", a, b, c, d)		// %d holds the value of int.
+}
